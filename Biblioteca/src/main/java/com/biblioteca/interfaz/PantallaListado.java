301f6d197441a00adbb2b80455f58c458eae8565
cgriffa <c4milogriffa@gmail.com>
2023-11-30 00:50:13 -0300

    Botones en vista listado

--- Biblioteca/src/main/java/com/biblioteca/interfaz/PantallaListado.java
+++ Biblioteca/src/main/java/com/biblioteca/interfaz/PantallaListado.java
@@ -3,6 +3,10 @@ package com.biblioteca.interfaz;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
+import com.biblioteca.dao.DAOFactory;
+import com.biblioteca.dao.LibroDAO;
+import com.biblioteca.models.Libro;
+
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,12 +17,17 @@ import java.sql.Statement;
 public class PantallaListado extends javax.swing.JFrame {
     private Connection connection;
     private JTable booksTable;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    DAOFactory bibliotecaDB = DAOFactory.getInstance();
 
     public PantallaListado() {
         initComponents();
         setupDatabaseConnection();
         createBooksTable();
         loadBooksData();
+        createButtons();
     }
 
     private void initComponents() {
@@ -28,7 +37,7 @@ public class PantallaListado extends javax.swing.JFrame {
 
     private void setupDatabaseConnection() {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", "root", "admin38");
         } catch (SQLException e) {
             e.printStackTrace();
             // Manejar cualquier error de conexión aquí
@@ -39,6 +48,57 @@ public class PantallaListado extends javax.swing.JFrame {
         booksTable = new JTable();
         JScrollPane scrollPane = new JScrollPane(booksTable);
         add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void createButtons() {
+      addButton = new JButton("Agregar");
+      editButton = new JButton("Editar");
+      deleteButton = new JButton("Eliminar");
+
+      JPanel buttonPanel = new JPanel();
+      buttonPanel.add(addButton);
+      buttonPanel.add(editButton);
+      buttonPanel.add(deleteButton);
+
+      add(buttonPanel, BorderLayout.SOUTH);
+
+      // Agregar ActionListener para cada botón
+      addButton.addActionListener(e -> {
+          // Lógica para agregar un libro
+          // Puede abrir una ventana nueva para agregar un libro
+          VentanaCrearLibro ventanaCrearLibro = new VentanaCrearLibro();
+          ventanaCrearLibro.setVisible(true);
+          ventanaCrearLibro.getGuardarButton().addActionListener(ev -> {
+          // Obtener los datos ingresados por el usuario
+          String nombre = ventanaCrearLibro.getNombreField().getText();
+          String autor = ventanaCrearLibro.getAutorField().getText();
+          // Aquí puedes guardar los datos en la base de datos o realizar otra lógica
+          // Por ejemplo:
+          try {
+            LibroDAO libroDAO = bibliotecaDB.getLibroDAO();
+            Libro libro = new Libro();
+            libro.setNombre(nombre);
+            libro.setIdGenero(1L);
+            libro.setEstado(true);
+            libro.setAutor(autor);
+            libroDAO.create(libro);
+            connection.close();
+          } catch (SQLException err) {
+            err.printStackTrace();
+            // Manejo de errores de conexión o consulta SQL
+          }
+        });
+      });
+
+      editButton.addActionListener(e -> {
+          // Lógica para editar un libro seleccionado en la tabla
+          // Puede abrir una ventana nueva para editar un libro
+      });
+
+      deleteButton.addActionListener(e -> {
+          // Lógica para eliminar un libro seleccionado en la tabla
+          // Puede mostrar un mensaje de confirmación antes de eliminar
+      });
     }
 
     private void loadBooksData() {
